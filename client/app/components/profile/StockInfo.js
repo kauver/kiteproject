2b9769311bd640d85773c3be878da1ba9ca7a7dc
kauver <kauver@cse.iitk.ac.in>
2021-02-27 09:55:56 +0530

    ready for deployment

--- client/app/components/profile/StockInfo.js
+++ client/app/components/profile/StockInfo.js
@@ -7,7 +7,7 @@ const StockInfo=(props)=> {
     const {
         data,
     } = props;
-    const ticker = data['Meta Data']['2. Symbol'];
+    const symbol = data['Meta Data']['2. Symbol'];
     const lastRefreshed = data['Meta Data']['3. Last Refreshed'];
     const timezone = data['Meta Data']['5. Time Zone'];
 
@@ -38,7 +38,7 @@ const StockInfo=(props)=> {
     return(
         <div>
             
-            <p>{ticker}</p>
+            <p>{symbol}</p>
             <p>{lastRefreshed}</p>
             <p>{timezone}</p>
             <br />
